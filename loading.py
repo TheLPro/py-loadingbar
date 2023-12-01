84e5cced2d8646669626a696874bd7b5dfddebf8
Leon <leon.potocki@gmail.com>
2023-12-01 09:56:13 +0100

    Add files via upload

---
+++ loading.py
@@ -0,0 +1,42 @@
+from termcolor import colored
+from time import sleep
+import os
+import random
+
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+    
+class Bar:
+        
+    b = "█"
+    d = "•"
+    c = "light_green"
+    s = random.randint(1, 5) / 100
+    current = 0
+
+    def box(box):
+        Bar.b = box
+    def dot(dot):
+        Bar.d = dot
+    def color(color):
+        Bar.c = color
+    def speed(min, max, divider):
+        Bar.speed = random.randint(min, max) / divider
+
+    def update(percent, last):
+        current = last
+        for i in range(percent - last + 1):
+            if current == 0 or current == 1:
+                boxes = 0
+                dots = 50
+            else:
+                boxes = round(current / 2)
+                dots = round((100 - current) / 2)
+            cls()
+            print(colored(f"Loading {current + 1}% {Bar.b*boxes}{Bar.d*dots}", Bar.c))
+            sleep(Bar.s)
+            current += 1
+            if current == 100:
+                return True
+Bar.update(100, 0)
